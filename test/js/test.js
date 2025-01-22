eacdd276bd2a9ad8eba4d67a6bb205762ee769ab
lazyzzzcat <lazyzzzcat@gmail.com>
2025-01-22 22:38:39 +0800

    del

--- test/js/test.js
+++
@@ -1,9 +0,0 @@
-
-document.querySelector(".button").addEventListener('click', home);
-function home() {  
-    let name = document.getElementById("name").value;
-    document.getElementById("name").style.display = "none";
-    let hello = `ПРИВЕТ, ${name}!`;
-    document.getElementById("hello").textContent = hello;
-}
-
